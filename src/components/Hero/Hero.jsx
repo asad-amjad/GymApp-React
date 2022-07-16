4cca5a7f926d09cbdd58af8b38291c8ce95ae7b1
asad-amjad <asad.amjad.pk@gmail.com>
2022-07-16 12:04:13 +0500

    header menu

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,9 @@
+import Header from "../Header/Header";
+import "./Hero.css";
+const Hero = () => {
+  return <div className="hero">
+    <div className="left-h"><Header/></div>
+    <div className="right-h">Right</div>
+  </div>;
+};
+export default Hero;
