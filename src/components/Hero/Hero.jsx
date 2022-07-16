3245d6991bb4846f90292f0dc9cc9080fb17d7de
asad-amjad <asad.amjad.pk@gmail.com>
2022-07-16 12:19:44 +0500

    best ad section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,18 @@
 import Header from "../Header/Header";
 import "./Hero.css";
 const Hero = () => {
-  return <div className="hero">
-    <div className="left-h"><Header/></div>
-    <div className="right-h">Right</div>
-  </div>;
+  return (
+    <div className="hero">
+      <div className="left-h">
+        <Header />
+
+        <div className="the-best-ad">
+          <div></div>
+          <span>the best fitness club in the town</span>
+        </div>
+      </div>
+      <div className="right-h">Right</div>
+    </div>
+  );
 };
 export default Hero;
