094246d84035a6248c09d8e814f83e94d33a987a
asad-amjad <asad.amjad.pk@gmail.com>
2022-07-17 22:06:56 +0500

    introduce Bars menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,37 @@
 import "./Header.css";
 import Logo from "../../assets/logo.png";
+import Bars from "../../assets/bars.png";
+import { useState } from "react";
 const Header = () => {
+  const mobile = window.innerWidth <= 768 ? true : false;
+  const [menOpened, setMenOpened] = useState(false);
   return (
     <div className="header">
       <img src={Logo} alt="" className="logo" />
-      <ul className="header-menu">
-        <li>Home</li>
-        <li>Programs</li>
-        <li>Why us</li>
-        <li>Plans</li>
-        <li>Testimonial</li>
-      </ul>
+      {menOpened === false && mobile === true ? (
+        <div
+          style={{
+            backgroundColor: "var(--appColor)",
+            padding: "0.5rem",
+            borderRadius: "5px",
+          }}
+          onClick={() => setMenOpened(true)}
+        >
+          <img
+            src={Bars}
+            alt=""
+            style={{ width: "1.5rem", height: "1.5rem" }}
+          />
+        </div>
+      ) : (
+        <ul className="header-menu">
+          <li onClick={() => setMenOpened(false)}>Home</li>
+          <li onClick={() => setMenOpened(false)}>Programs</li>
+          <li onClick={() => setMenOpened(false)}>Why us</li>
+          <li onClick={() => setMenOpened(false)}>Plans</li>
+          <li onClick={() => setMenOpened(false)}>Testimonial</li>
+        </ul>
+      )}
     </div>
   );
 };
