b617711795a53f8558fc162ac1218f87bf81fb75
asad-amjad <asad.amjad.pk@gmail.com>
2022-07-17 15:41:57 +0500

    plans section

---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,39 @@
+import { plansData } from "../../data/plansData";
+import whiteTick from "../../assets/whiteTick.png";
+import "./Plans.css";
+const Plans = () => {
+  return (
+    <div className="plans-container">
+      <div className="programs-header" style={{ gap: "2rem" }}>
+        <span className="stroke-text">Ready to start</span>
+        <span>Your journey</span>
+        <span className="stroke-text">now with us</span>
+      </div>
+
+      {/* plans card */}
+      <div className="plans">
+        {plansData.map((plan, i) => (
+          <div key={i} className="plan">
+            {plan.icon}
+            <span>{plan.name}</span>
+            <span>$ {plan.price}</span>
+            <div className="features">
+              {plan.features.map((feature, i) => (
+                <div className="feature">
+                  <img src={whiteTick} alt="" />
+                  <span key={i}>{feature}</span>
+                </div>
+              ))}
+            </div>
+
+            <div>
+              <span>See more benifits -></span>
+            </div>
+            <button className="btn"> Join Now</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Plans;
