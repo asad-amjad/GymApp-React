c5f4d6caacf054cd356b50a53263b031416a8898
asad-amjad <asad.amjad.pk@gmail.com>
2022-07-17 10:09:34 +0500

    program categories

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,3 +1,5 @@
+import { programsData } from "../../data/programsData";
+import RightArrow from "../../assets/rightArrow.png";
 import "./Programs.css";
 
 const Programs = () => {
@@ -7,6 +9,20 @@ const Programs = () => {
         <span className="stroke-text">Explore our</span>
         <span>Programs</span>
         <span className="stroke-text">to shape you</span>
+      </div>
+
+      <div className="programs-categories">
+        {programsData.map((program) => (
+          <div className="category">
+            {program.image}
+            <span>{program.heading}</span>
+            <span>{program.details}</span>
+            <div className="join-now">
+              <span>Join Now</span>
+              <img src={RightArrow} alt="" />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
